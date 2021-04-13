f5a4eb3709ee6cb6105a9e63762a9daf2d05478c
Om Rohith Karamala <rohith@Oms-MacBook-Pro.hsd1.md.comcast.net>
2021-04-13 14:08:02 -0400

    My solution to simple Add two numbers problem.

---
+++ java/FLOW001.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Codechef {
+    public static void main(String [] args) {
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int t = Integer.parseInt(br.readLine());
+
+            for(int a = 0; a < t; a++) {
+                String [] line = br.readLine().split(" ");
+                int one = Integer.parseInt(line[0]);
+                int two = Integer.parseInt(line[1]);
+                int result = one + two;
+
+                System.out.println(result);
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
