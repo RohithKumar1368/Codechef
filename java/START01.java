e70a63a6ede4af59df3f32cf54f653205488953e
Om Rohith Karamala <rohith@Oms-MacBook-Pro.hsd1.md.comcast.net>
2021-04-14 18:01:29 -0400

    START01 problem

---
+++ java/START01.java
@@ -0,0 +1,17 @@
+/*
+    Problem Description: https://www.codechef.com/problems/START01
+*/
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Codechef {
+    public static void main(String [] args) {
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println(br.readLine());
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
